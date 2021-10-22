cb0e505d83740b3c4de4cdd8f738fd8d7ea0ba4f
WillDevKenzie <91714764+WillDevKenzie@users.noreply.github.com>
2021-10-22 00:34:58 -0300

    SnapCrackleDelivery

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,28 @@
-// Seu código vai aqui
+function snapCrackle(maxValue){
+    let texto= ""
+    for (let numero=1; numero <= maxValue; numero++){
+    
+        if ((numero % 2 ==0) && (numero % 5 !==0)){
+        texto += numero +","
+    }    
+    
+    if ((numero % 2 !==0) && (numero % 5 ==0)){
+        texto += " SnapCrackle, "
+    }
+    
+    else if (numero % 2 !==0){
+        texto += " Snap, "
+    }
+    
+    else if (numero % 5 ==0){
+        texto += " Crackle, "
+    }
+    
+    
+    
+    
+}
+console.log(texto)
+}
+snapCrackle(15)
+    
