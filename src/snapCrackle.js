ae213b956338dde495659959f39021088a971c7f
WillDevKenzie <91714764+WillDevKenzie@users.noreply.github.com>
2021-10-25 01:14:26 -0300

    SnapCrackleDelivery

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,28 +1,49 @@
 function snapCrackle (maxValue){
-    let texto = ""
+    function primo (maxValue){
+        let divisores = 0
+        for(let num = 1; num <= maxValue; num++){
+            if(maxValue % num === 0){
+               divisores++ 
+            }
+        }
+        if (divisores === 2){
+            return true
+        }
+        else{
+            return false
+        }
+
+    }          
+    let arr = []
     for (let numero=1; numero <= maxValue; numero++){
     
-        if ((numero % 2 ==0) && (numero % 5 !==0)){
-        texto += numero +","
+        if ((numero % 2 ==0) && (numero % 5 !==0) && !(primo(numero))){
+        arr.push (numero) 
     }    
     
-    if ((numero % 2 !==0) && (numero % 5 ==0)){
-        texto += " SnapCrackle, "
+    if ((numero % 2 !== 0) && (numero % 5 ==0) && (primo(numero))){
+        arr.push ('SnapCracklePrime')
     }
-    
+    else if (primo(numero) && (numero % 2 !== 0)){
+        arr.push ('SnapPrime')
+    }
+    else if ((numero % 2 !== 0) && (numero % 5 ==0)){
+        arr.push ('SnapCrackle')
+    }
     else if (numero % 2 !==0){
-        texto += " Snap, "
+        arr.push ('Snap')
     }
     
     else if (numero % 5 ==0){
-        texto += " Crackle, "
+        arr.push ('Crackle')
     }
-    
-    
-    
-    
+    else if (primo(numero)){
+        arr.push ('Prime')
+    }    
 }
-console.log(texto)
+console.log(`${arr}`)
 }
 snapCrackle(15)
+
+
     
